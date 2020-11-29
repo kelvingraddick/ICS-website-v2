976ece31c95f5b1ed65a7b2c1892a40d56988fc7
Kelvin Graddick <kelvingraddick@kg.codes>
2020-11-29 00:30:07 -0500

    Contact form integration with MailChimp

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,7 +3,7 @@
 		<div class="relative bg-white">
       <div class="lg:absolute lg:inset-0">
         <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-          <img class="w-full object-contain rounded-md" src="ics-image-10.png" alt="">
+          <img class="w-full object-contain rounded-md" src="ics-image-14.png" alt="">
         </div>
       </div>
       <div class="relative pt-12 pb-16 px-4 sm:pt-16 lg:pt-0 sm:px-6 lg:px-0 lg:max-w-7xl lg:mx-auto lg:grid lg:grid-cols-2">
@@ -41,29 +41,29 @@
                 </dd>
               </div>
             </dl>
-            <form action="#" method="POST" class="mt-9 grid grid-cols-1 row-gap-6 sm:grid-cols-2 sm:col-gap-8">
+            <form action="https://icscanhelp.us7.list-manage.com/subscribe/post?u=285c6f1164e3bd8c329e8bb36&amp;id=3e68db4c52" method="POST" target="_blank" novalidate class="mt-9 grid grid-cols-1 row-gap-6 sm:grid-cols-2 sm:col-gap-8">
               <div>
                 <label for="first_name" class="block text-sm font-medium leading-5 text-gray-700">First name</label>
                 <div class="mt-1 relative rounded-md shadow-sm">
-                  <input id="first_name" class="form-input block w-full transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                  <input id="first_name" name="FNAME" class="form-input block w-full transition ease-in-out duration-150 sm:text-sm sm:leading-5">
                 </div>
               </div>
               <div>
                 <label for="last_name" class="block text-sm font-medium leading-5 text-gray-700">Last name</label>
                 <div class="mt-1 relative rounded-md shadow-sm">
-                  <input id="last_name" class="form-input block w-full transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                  <input id="last_name" name="LNAME" class="form-input block w-full transition ease-in-out duration-150 sm:text-sm sm:leading-5">
                 </div>
               </div>
               <div class="sm:col-span-2">
                 <label for="email" class="block text-sm font-medium leading-5 text-gray-700">Email</label>
                 <div class="mt-1 relative rounded-md shadow-sm">
-                  <input id="email" type="email" class="form-input block w-full transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                  <input id="email" name="EMAIL" type="email" class="form-input block w-full transition ease-in-out duration-150 sm:text-sm sm:leading-5">
                 </div>
               </div>
               <div class="sm:col-span-2">
                 <label for="company" class="block text-sm font-medium leading-5 text-gray-700">Company</label>
                 <div class="mt-1 relative rounded-md shadow-sm">
-                  <input id="company" class="form-input block w-full transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                  <input id="company" name="COMPANY" class="form-input block w-full transition ease-in-out duration-150 sm:text-sm sm:leading-5">
                 </div>
               </div>
               <div class="sm:col-span-2">
@@ -72,7 +72,7 @@
                   <span class="text-sm leading-5 text-gray-500">Optional</span>
                 </div>
                 <div class="mt-1 relative rounded-md shadow-sm">
-                  <input id="phone" class="form-input block w-full transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                  <input id="phone" name="PHONE" class="form-input block w-full transition ease-in-out duration-150 sm:text-sm sm:leading-5">
                 </div>
               </div>
               <div class="sm:col-span-2">
@@ -81,7 +81,7 @@
                   <span class="text-sm leading-5 text-gray-500">Max. 500 characters</span>
                 </div>
                 <div class="mt-1 relative rounded-md shadow-sm">
-                  <textarea id="how_can_we_help" rows="4" class="form-textarea block w-full transition ease-in-out duration-150 sm:text-sm sm:leading-5"></textarea>
+                  <textarea id="how_can_we_help" name="MESSAGE" rows="4" class="form-textarea block w-full transition ease-in-out duration-150 sm:text-sm sm:leading-5"></textarea>
                 </div>
               </div>
               <fieldset class="sm:col-span-2">
@@ -90,25 +90,25 @@
                 </legend>
                 <div class="mt-4 grid grid-cols-1 row-gap-4">
                   <div class="flex items-center">
-                    <input id="budget_under_25k" name="budget" value="under_25k" type="radio" class="form-radio h-4 w-4 text-orange-600 transition duration-150 ease-in-out">
+                    <input id="budget_under_25k" name="BUDGET" value="Less than $25K" type="radio" class="form-radio h-4 w-4 text-orange-600 transition duration-150 ease-in-out">
                     <label for="budget_under_25k" class="ml-3">
                       <span class="block text-sm leading-5 text-gray-700">Less than $25K</span>
                     </label>
                   </div>
                   <div class="flex items-center">
-                    <input id="budget_25k-50k" name="budget" value="25k-50k" type="radio" class="form-radio h-4 w-4 text-orange-600 transition duration-150 ease-in-out">
+                    <input id="budget_25k-50k" name="BUDGET" value="$25K - $50K" type="radio" class="form-radio h-4 w-4 text-orange-600 transition duration-150 ease-in-out">
                     <label for="budget_25k-50k" class="ml-3">
                       <span class="block text-sm leading-5 text-gray-700">$25K – $50K</span>
                     </label>
                   </div>
                   <div class="flex items-center">
-                    <input id="budget_50k-100k" name="budget" value="50k-100k" type="radio" class="form-radio h-4 w-4 text-orange-600 transition duration-150 ease-in-out">
+                    <input id="budget_50k-100k" name="BUDGET" value="$50K - $100K" type="radio" class="form-radio h-4 w-4 text-orange-600 transition duration-150 ease-in-out">
                     <label for="budget_50k-100k" class="ml-3">
                       <span class="block text-sm leading-5 text-gray-700">$50K – $100K</span>
                     </label>
                   </div>
                   <div class="flex items-center">
-                    <input id="budget_over_100k" name="budget" value="over_100k" type="radio" class="form-radio h-4 w-4 text-orange-600 transition duration-150 ease-in-out">
+                    <input id="budget_over_100k" name="BUDGET" value="$100K+" type="radio" class="form-radio h-4 w-4 text-orange-600 transition duration-150 ease-in-out">
                     <label for="budget_over_100k" class="ml-3">
                       <span class="block text-sm leading-5 text-gray-700">$100K+</span>
                     </label>
@@ -118,10 +118,15 @@
               <div class="sm:col-span-2">
                 <label for="how_did_you_hear_about_us" class="block text-sm font-medium leading-5 text-gray-700">How did you hear about us?</label>
                 <div class="mt-1 relative rounded-md shadow-sm">
-                  <input id="how_did_you_hear_about_us" class="form-input block w-full transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                  <input id="how_did_you_hear_about_us" name="SOURCE" class="form-input block w-full transition ease-in-out duration-150 sm:text-sm sm:leading-5">
                 </div>
               </div>
               <div class="text-right sm:col-span-2">
+                <div id="mce-responses" class="clear">
+                  <div class="response" id="mce-error-response" style="display:none"></div>
+                  <div class="response" id="mce-success-response" style="display:none"></div>
+                </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_285c6f1164e3bd8c329e8bb36_3e68db4c52" tabindex="-1" value=""></div>
                 <span class="inline-flex rounded-md shadow-sm">
                   <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-orange-500 hover:bg-orange-500 focus:outline-none focus:border-orange-700 focus:shadow-outline-orange active:bg-orange-700 transition duration-150 ease-in-out">
                     Submit
